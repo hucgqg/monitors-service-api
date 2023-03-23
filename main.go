5ffab8272ca797b25fa29d860c9ee90395b19bd2
changqing <hucgqg@qq.com>
2023-03-23 09:41:27 +0800

    增加端口自定义到配置文件

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"monitors-service-api-gitee/routers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -14,17 +15,21 @@ func main() {
 	// 初始化配置文件
 	models.InitConfig()
 
+	// 路由注册
 	r := gin.Default()
 	routers.FeishuRouterInit(r)
 	routers.DingRouterInit(r)
 	routers.FlinkRouterInit(r)
 
+	// 启动Flink定时监控
 	go func() {
 		flink.TickerMonitor()
 	}()
 
-	err := r.Run()
-	if err != nil {
-		return
+	// 启动时端口设置
+	if viper.GetString("port") == "" {
+		r.Run()
+	} else {
+		r.Run(":" + viper.GetString("port"))
 	}
 }
